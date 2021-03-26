49a833357e43990c7ddd8e4356c4bcebcbe3514d
Willian vinicius Maruyama <willianviniciusmaruyama@Willians-MacBook-Pro.local>
2021-03-26 15:50:32 +0900

    quick refactor

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const passportLocalMongoose = require('passport-local-mongoose');
 const User = require('./models/user');
 const session = require('express-session');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 
 
 //require routes
@@ -44,6 +45,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'));
 
 // configure Passport and sessions
 app.use(session({
