384a6e3b6050b571c588138babed763f60c4ddec
Willian vinicius Maruyama <willianviniciusmaruyama@Willians-MacBook-Pro.local>
2021-03-26 10:31:44 +0900

    add login and lougout

--- app.js
+++ app.js
@@ -51,10 +51,10 @@ app.use(session({
   resave: false,
   saveUninitialized: true,
 }));
+app.use(passport.initialize());
+app.use(passport.session());
 
-// CHANGE: USE "createStrategy" INSTEAD OF "authenticate"
 passport.use(User.createStrategy());
-
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
--- routes/index.js
+++ routes/index.js
@@ -1,59 +1,69 @@
 const express = require('express');
 const router = express.Router();
-const {postRegister} = require('../controllers/index');
+const passport = require('passport');
+const { postRegister } = require('../controllers/index');
+const { errorHandler } = require('../middleware/index');
 
 /* GET home page. */
-router.get('/', (req, res, next)  => {
+router.get('/', (req, res, next) => {
   res.render('index', { title: 'Surf Shop - Home' });
 });
 
 /* GET /register. */
-router.get('/register', (req, res, next)  => {
+router.get('/register', (req, res, next) => {
   res.send('GET /register');
 });
 
 
 /* POST /register. */
-router.post('/register', postRegister);
+router.post('/register', errorHandler(postRegister));
 
 
 /* GET /login. */
-router.get('/login', (req, res, next)  => {
+router.get('/login', (req, res, next) => {
   res.send('GET /login');
 });
 
-/* GET /login. */
-router.post('/login', (req, res, next)  => {
-  res.send('POST /login');
+/* POST /login. */
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login'
+}));
+
+//GET /logout.
+router.get('/logout', (req, res, next) => {
+  req.logout();
+  res.redirect('/');
 });
 
+
 /* GET /profile. */
-router.get('/profile', (req, res, next)  => {
+router.get('/profile', (req, res, next) => {
   res.send('GET /profile');
 });
 
 /* PUT /profile/:user_id. */
-router.put('/profile', (req, res, next)  => {
+router.put('/profile', (req, res, next) => {
   res.send('PUT /profile/:user_id');
 });
 
 /* GET /forgot. */
-router.get('/forgot', (req, res, next)  => {
+router.get('/forgot', (req, res, next) => {
   res.send('GET /forgot');
 });
 
 /* PUT /forgot. */
-router.put('/forgot', (req, res, next)  => {
+router.put('/forgot', (req, res, next) => {
   res.send('PUT /forgot');
 });
 
 /* GET /reset/:token. */
-router.get('/reset/:token', (req, res, next)  => {
+router.get('/reset/:token', (req, res, next) => {
   res.send('GET /reset/:token');
 });
 
 /* PUT /reset/:token. */
-router.put('/reset/:token', (req, res, next)  => {
+router.put('/reset/:token', (req, res, next) => {
   res.send('PUT /reset/:token');
 });
 
