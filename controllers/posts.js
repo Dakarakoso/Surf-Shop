e092ee69704bf8cd749bce7d24466fb9371e2dfd
Willian vinicius Maruyama <willianviniciusmaruyama@Willians-MacBook-Pro.local>
2021-03-30 03:46:28 +0900

    add upload to edit images

--- controllers/posts.js
+++ controllers/posts.js
@@ -7,48 +7,84 @@ cloudinary.config({
 });
 
 module.exports = {
-    // Posts index
+    // Posts Index
     async postIndex(req, res, next) {
         let posts = await Post.find({});
         res.render('posts/index', { posts });
     },
-    // Posts new
+    // Posts New
     postNew(req, res, next) {
         res.render('posts/new');
     },
-    //Posts create
+    // Posts Create
     async postCreate(req, res, next) {
         req.body.post.images = [];
-        for (let file of req.files) {
+        for (const file of req.files) {
             let image = await cloudinary.v2.uploader.upload(file.path);
             req.body.post.images.push({
                 url: image.secure_url,
-                public_id: image.public
+                public_id: image.public_id
             });
         }
         let post = await Post.create(req.body.post);
         res.redirect(`/posts/${post.id}`);
     },
-
-    //Posts show
+    // Posts Show
     async postShow(req, res, next) {
         let post = await Post.findById(req.params.id);
         res.render('posts/show', { post });
     },
-    //Posts edit
+    // Posts Edit
     async postEdit(req, res, next) {
         let post = await Post.findById(req.params.id);
         res.render('posts/edit', { post });
     },
-    //Posts update
+    // Posts Update
     async postUpdate(req, res, next) {
-        let post = await Post.findByIdAndUpdate(req.params.id, req.body.post);
+        // find the post by id
+        let post = await Post.findById(req.params.id);
+        // check if there's any images for deletion
+        if (req.body.deleteImages && req.body.deleteImages.length) {
+            // assign deleteImages from req.body to its own variable
+            let deleteImages = req.body.deleteImages;
+            // loop over deleteImages
+            for (const public_id of deleteImages) {
+                // delete images from cloudinary
+                await cloudinary.v2.uploader.destroy(public_id);
+                // delete image from post.images
+                for (const image of post.images) {
+                    if (image.public_id === public_id) {
+                        let index = post.images.indexOf(image);
+                        post.images.splice(index, 1);
+                    }
+                }
+            }
+        }
+        // check if there are any new images for upload
+        if (req.files) {
+            // upload images
+            for (const file of req.files) {
+                let image = await cloudinary.v2.uploader.upload(file.path);
+                // add images to post.images array
+                post.images.push({
+                    url: image.secure_url,
+                    public_id: image.public_id
+                });
+            }
+        }
+        // update the post with any new properties
+        post.title = req.body.post.title;
+        post.description = req.body.post.description;
+        post.price = req.body.post.price;
+        post.location = req.body.post.location;
+        // save the updated post into the db
+        post.save();
+        // redirect to show page
         res.redirect(`/posts/${post.id}`);
     },
-    // Posts destroy
+    // Posts Destroy
     async postDestroy(req, res, next) {
-        let post = await Post.findByIdAndRemove(req.params.id);
+        await Post.findByIdAndRemove(req.params.id);
         res.redirect('/posts');
     }
 }
-
--- routes/posts.js
+++ routes/posts.js
@@ -30,7 +30,7 @@ router.get('/:id/edit', asyncErrorHandler(postEdit));
 
 
 /* PUT posts update /posts/:id */
-router.put('/:id', asyncErrorHandler(postUpdate));
+router.put('/:id', upload.array('images', 4), asyncErrorHandler(postUpdate));
 
 /* GET posts destroy /posts/:id */
 router.delete('/:id', asyncErrorHandler(postDestroy));
