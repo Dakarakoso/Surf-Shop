14d7a92f019b2bdfb311cc38d1ed8e19de9e8967
Willian Negishi <willian_jvm@hotmail.com>
2021-05-20 06:07:11 +0900

    Try to deploy

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-require('dotenv').config();
+if (process.env.NODE_ENV !== "production") {
+  require('dotenv').config();
+}
 
 const express = require('express');
 const engine = require('ejs-mate');
@@ -6,11 +8,15 @@ const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
+const flash = require('connect-flash');
 const passport = require('passport');
 const User = require('./models/user');
 const session = require('express-session');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
+const helmet = require('helmet');
+const dbUrl = process.env.DB_URL ||  'mongodb://localhost:27017/surf-shop';
+
 // const seedPosts = require('./seeds');
 // seedPosts();
 
@@ -21,8 +27,6 @@ const reviews = require('./routes/reviews');
 
 const app = express();
 //connect to the database
-const dbUrl = 'mongodb://localhost:27017/surf-shop';
-
 mongoose.connect(dbUrl, {
   useNewUrlParser: true,
   useCreateIndex: true,
@@ -43,6 +47,7 @@ app.set('view engine', 'ejs');
 // set public assets directory
 app.use(express.static('public'));
 
+const secret = process.env.SECRET || 'thisshouldbeabettersecret!';
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -51,14 +56,74 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'));
+app.use(flash());
+
+const scriptSrcUrls = [
+  "https://stackpath.bootstrapcdn.com/",
+  "https://api.tiles.mapbox.com/mapbox-gl-js/v0.51.0/mapbox-gl.js",
+  "https://api.mapbox.com/",
+  "https://kit.fontawesome.com/",
+  "https://cdnjs.cloudflare.com/",
+  "https://use.fontawesome.com/releases/v5.15.3/js/all.js", 
+  "https://code.jquery.com/jquery-3.3.1.slim.min.",
+  "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js",
+  "https://code.jquery.com/jquery-3.3.1.slim.min.js"
+];
+const styleSrcUrls = [
+  "https://kit-free.fontawesome.com/",
+  "https://stackpath.bootstrapcdn.com/",
+  "https://api.mapbox.com/",
+  "https://api.tiles.mapbox.com/",
+  "https://fonts.googleapis.com/",
+  "https://use.fontawesome.com/",
+  "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+];
+const connectSrcUrls = [
+  "https://api.mapbox.com/",
+  "https://*.tiles.mapbox.com",
+  "https://events.mapbox.com/",
+];
+const fontSrcUrls = [
+  "https://fonts.gstatic.com/s/montserrat/v15/JTURjIg1_i6t8kCHKm45_cJD3gTD_vx3rCubqg.woff2",
+  "https://fonts.gstatic.com/s/montserrat/v15/JTURjIg1_i6t8kCHKm45_cJD3g3D_vx3rCubqg.woff2"
+];
+app.use(
+  helmet.contentSecurityPolicy({
+      directives: {
+          defaultSrc: ["http://localhost:3000/"],
+          connectSrc: ["'self'", ...connectSrcUrls],
+          scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+          styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+          workerSrc: ["'self'", "blob:"],
+          childSrc: ["blob:"],
+          objectSrc: [],
+          imgSrc: [
+              "'self'",
+              "blob:",
+              "data:",
+              `https://res.cloudinary.com/${process.env.CLOUDINARY_NAME}/`, 
+            "https://images.unsplash.com/",
+              "https://res.cloudinary.com/devsprout/image/upload/v1561315599/surf-shop/surfboard.jpg"
+          ],
+      fontSrc: ["'self'",...fontSrcUrls],
+      },
+  })
+);
 
 // Add moment to ervy view
 app.locals.moment = require('moment');
 // Configure Passport and Sessions
 app.use(session({
-  secret: 'hang ten dude!',
+  name: 'session',
+  secret,
   resave: false,
-  saveUninitialized: true
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    // secure: true,
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7
+  }
 }));
 
 app.use(passport.initialize());
